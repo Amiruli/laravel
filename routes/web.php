b7c8832d2871822c093eba172702c412689cbf72
Amiruli <amirul.ei@gmail.com>
2020-08-12 16:35:16 +0600

    Create all other pages: blog, contact, login

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,30 @@ Route::get('/about', function(){
     return view('pages.about');
 });
 
+Route::get('/contact', function(){
+    return view('pages.contact');
+});
+
+Route::get('/blog', function(){
+    return view('pages.blog');
+});
+
+Route::get('post', function(){
+    return view('pages.single_blog');
+});
+
+Route::get('/login', function(){
+    return view('pages.login');
+});
+
+Route::get('/shop', function(){
+    return view('pages.ecommerce.shop');
+});
+
+Route::get('/product', function(){
+    return view('pages.ecommerce.single_product');
+});
+
 Route::get('/cart', function(){
     return view('pages.ecommerce.cart');
 });
@@ -29,12 +53,14 @@ Route::get('/checkout', function(){
     return view('pages.ecommerce.checkout');
 });
 
-Route::get('', function(){
-    return view('pages.ecommerce.tracking');
-});
-
 Route::get('/thankyou', function(){
     return view('pages.ecommerce.thankyou');
 });
+
+Route::get('track-order', function(){
+    return view('pages.ecommerce.tracking');
+});
+
+
 
 
