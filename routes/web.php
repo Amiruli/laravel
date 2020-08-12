21db55183b5602af2e07fce02148328f91f367c4
Amiruli <amirul.ei@gmail.com>
2020-08-12 15:14:40 +0600

    installed HTML template

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.index');
 });
 
-Route::get('/', function (){
-    return "hello";
+Route::get('/about', function(){
+    return view('pages.about');
 });
